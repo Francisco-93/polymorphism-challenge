4dc046b43dd7813b93a81b588da58642643447f2
francisco.aguiar <francisco.aguiar@MDR31548.integracao.gov.br>
2020-10-27 15:11:13 -0300

    Projeto criado

---
+++ polimorfismo2/src/entities/ImportedProduct.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class ImportedProduct extends Product{
+	
+	private Double customsFee;
+	
+	public ImportedProduct() {
+	}
+	
+	public ImportedProduct(String name, Double price, Double customsFee) {
+		super(name, price);
+		this.customsFee = customsFee;
+	}
+
+	public Double getCustomsFee() {
+		return customsFee;
+	}
+
+	public void setCustomsFee(Double customsFee) {
+		this.customsFee = customsFee;
+	}
+	
+	@Override
+	public String priceTag() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(super.name + " $" + String.format("%.2f", super.price) + " (Customs fee: $ " + String.format("%.2f", customsFee) + ")");
+		return sb.toString();
+	}
+}
